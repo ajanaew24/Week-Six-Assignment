8f8b63d017b1727c9b4dd982ba9a87a4fda6dda4
ajanaew24 <ajw10@att.net>
2015-10-28 18:50:51 +0000

    Changed stuff.

--- GOLV1.py
+++ GOLV1.py
@@ -1,41 +1,111 @@
-#Alicia Williams
-#CIS-125 FA 2015
-#Week 6 Assignment
-#File: GameofLife.py
+#Ali# Framework for Conway's game of life
+# Alicia Williams
+# CIS 125 FA2015
+# Week 7 Assignment
+# Collaborations with Rebekah Orth and Dr. Neumann
 
-import random
+# Functions
+# populate()
+# display()
+# generation()
+# main()
 
-world = []
-height = 80
-width = 22
+#Function populate
+#preconditions
+#   random is imported
+#   world is created as list
+#   height is defined (as integer)
+#   width is defined (as integer)
+#postcondition
+#   world is populated with ?? (string)
 
 
-#Function Populate
-#Precondtiitons
-#world is  list
-
-#Postconditions
-#world will contain random cells
-def populate(stuff, h = height, w = width):
+def rpopulate(petri_dish, h=80, w=22):
+    import random
     for x in range(h):
-        row = []
-        for y in range(w):
-            row.append(random.randint(0,1))
-        stuff.append(row)
+            row = []
+            for y in range(w):
+                    row.append(random.randint(0, 1))
+            petri_dish.append(row)
 
-#Function display
-#Precondition
+def populate(petri_dish, h=80, w=22):
+    for x in range(h):
+            row = []
+            for y in range(w):
+                    row.append(0)
+            petri_dish.append(row)
+
+# Function display(world,h,w)
+# Preconditions
 #   world is populated
-#postcondition none
-def display(cheese, h = height, w = width):
-    worldString = ""
+# Postcondition
+#   world is not changed
+
+
+def display(world, h = 22, w = 80):
+    worldstring = ""
     for x in range(h):
-        rowString = ""
         for y in range(w):
-            if cheese[x][y] == 0:
-                rowString += " "
+            if world[x][y] == 1:
+                worldstring += "*"
             else:
-                rowString += "*"
-        worldString += rowString + "\n"
-        print(worldString)
+                worldstring += " "
+        worldstring += '\n'
+    print(worldstring)
+
+
+# Function generation(world,h,w)
+# Preconditions
+#   world is populated
+# Postconditions
+#   Returns new world
+
+
+def generation(petri_dish, h=22, w=80):
+    new_world = []
+    populate(new_world, h, w)
+    
+    n = 0    
+    for x in range(1,h-1):
+        for y in range(1,w-1):
+            n = petri_dish[x-1][y-1] +  \
+                petri_dish[x-1][y] +  \
+                petri_dish[x-1][y+1] +  \
+                petri_dish[x][y-1] +  \
+                petri_dish[x][y+1] +  \
+                petri_dish[x+1][y-1] +  \
+                petri_dish[x+1][y] +  \
+                petri_dish[x+1][y+1]
+
             
+            if petri_dish[x][y] == 0:
+                if n == 3:
+                    new_world[x][y] = 1
+                else:
+                    new_world[x][y] = 0
+            else: #(cell is alive)
+                if n < 2 or n > 3:
+                    new_world[x][y] = 0
+                else:
+                    new_world[x][y] = 1 
+    
+    return new_world
+
+
+def main():
+    world = []
+    height = 22
+    width = 80
+    rpopulate(world, height, width)
+    display(world, height, width)
+    key = input("Press q to quit, any other key to continue: ")
+    while key != 'q':
+        world = generation(world, height, width)
+        display(world, height, width)
+        key = input("Press q to quit, any other key to continue: ")
+
+    print("Goodbye")
+
+
+if __name__ == '__main__':
+    main()
